3a1fa30d0c0b864bd9150041cd0bb5e43f076e31
P Patel <ppatel@Tejs-MacBook-Air.local>
2025-06-03 16:40:35 -0400

    Almost done
    git push

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,27 +1,63 @@
-print("Welcome to Tic Tac Toe!")
-
-userChoice= input("Do you wanna be player O or player X?").lower()
-
-s1="1"
-s2="2"
-s3="3"
-s4="4"
-s5="5"
-s6="6"
-s7="7"
-s8="8"
-s9="9"
-
-row1= [s1, s2,s3]
-row2=[s4,s5,s6]
-row3=[s7,s8,s9]
-
-def board():
-    print(f"A  {s1} | {s2} | {s3}")
-    print("  ---+---+---")
-    print(f"B  {s4} | {s5} | {s6}")
-    print("  ---+---+---")
-    print(f"C  {s7} | {s8} | {s9}")
+from random import randint
 
 
-board()
+print("Welcome to the TicTacToe game!");
+
+userFigureChoice = input('Do you want to be X or O: ');
+userChoice = ''
+
+
+grids = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+
+
+def createBoard():
+    print(f"  {grids[0]} | {grids[1]} | {grids[2]}")
+    print("  --+---+---")
+    print(f"  {grids[3]} | {grids[4]} | {grids[5]}")
+    print("  --+---+---")
+    print(f"  {grids[6]} | {grids[7]} | {grids[8]}")
+
+    
+def get_userChoice():
+        while True:
+             userChoice = int(input("Choose a square from 1-9: "))
+             if(grids[userChoice-1].lower() == 'x' or grids[userChoice-1].lower() == "o"):
+                 userChoice = int(input("Choose another square!"));
+                 continue;
+             else:
+                grids[userChoice-1] = userFigureChoice;
+                break;
+    
+
+      
+
+
+
+
+
+def get_computerChoice():
+    computerChoice = 'O' if userFigureChoice.lower() == 'x' else 'X'
+    
+    while True:
+        index = randint(0,8)
+        if grids[index].lower() not in ['x', 'o']:
+            grids[index] = computerChoice
+            break
+        else:
+            print("Computer picked a taken spot, retrying...")
+
+        
+
+def play():
+    if(userFigureChoice.lower() == 'x'):
+        while True:
+            createBoard();
+            get_userChoice();
+            get_computerChoice()
+    else:
+        while True:
+            createBoard()
+            get_computerChoice();
+            get_userChoice();
+
+play();
